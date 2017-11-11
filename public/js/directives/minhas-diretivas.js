195e55b93643c5b7af859e3cafa561e81ac2f5e3
bfdegani <bruno.degani@gmail.com>
2017-11-11 02:06:11 -0200

    meu-painel alterado para usar transclude para inserir img como elemento filho do painel

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -5,8 +5,8 @@ angular.module('minhasDiretivas', [])
     ddo.restrict="AE"; //A = Atribute, E = Element. AE restringe essa diretiva para uso em atributos e elemtnos
 
     ddo.scope = { //definição do escopo da diretiva
-        titulo: '@titulo', // @titulo significa que o parametro titulo no codigo html será atribuido para esse elemento
-        url: '@url' //em vez de @titulo, pode ser informado apenas @ e o angular assume que o nome do atributo html será o mesmo do elemento do escopo da diretiva
+        titulo: '@titulo' // @titulo significa que o parametro titulo no codigo html será atribuido para esse elemento
+        //,url: '@url' //em vez de @titulo, pode ser informado apenas @ e o angular assume que o nome do atributo html será o mesmo do elemento do escopo da diretiva
     }
 
     ddo.transclude = true; //preserva elementos filhos no DOM
@@ -15,8 +15,7 @@ angular.module('minhasDiretivas', [])
     + '    <div class="panel-heading">'
     + '      <h3 class="panel-title text-center">{{titulo}}</h3>'
     + '    </div>'
-    + '    <div class="panel-body"> '
-    + '      <img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">'
+    + '    <div class="panel-body" ng-transclude> <!-- ng-transclude indica onde os elementos filhos serão inseridos -->'
     + '    </div>'
     + '</div> <!-- fim panel -->';
 
